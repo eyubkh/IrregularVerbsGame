59c4f189d764fc1319370ea73e70202367908144
Eyub <eyub@unknown.home>
2021-12-19 20:11:08 +0000

    backend cros added and fixed some problems

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const DATA = require('./data/verbs-es.json')
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
 app.get('/api/', (req, res) => {
   res.status(200).send(DATA)
 })
